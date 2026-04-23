c4c992f4a9de0fbae0dd7e9b96762da63f321567
pmvroque15 <pmvroque@gmail.com>
2026-04-22 21:53:00 -0400

    Sorted the ArrayList of Products by its name

---
+++ src/main/java/com/pluralsight/Scratch.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class Scratch {
+
+    static void main() {
+        System.out.println(add(1,2));;
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,31 +1,38 @@
 package com.pluralsight;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class StoreApp {
 
     static void main(String[] args) {
         ArrayList<Product> inventory = getInventory();
-
         System.out.println("We carry the following inventory: ");
+        Collections.sort(inventory, Comparator.comparing(Product::getName));
         for (int i = 0; i < inventory.size(); i++) {
             Product p = inventory.get(i);
             System.out.printf("id: %d %s - Price: $%.2f%n",
                     p.getId(), p.getName(), p.getPrice());
         }
+
     }
 
     public static ArrayList<Product> getInventory() {
+
         ArrayList<Product> inventory = new ArrayList<Product>();
 
         //Reads file line by line
         try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader("src/main/resources/inventory.csv"));
-            String line = " ";
+            FileReader fileReader = new FileReader("src/main/resources/inventory.csv");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line = "";
 
+            //4567|10' 2x4 (grade B)|9.99
             while ((line = bufferedReader.readLine()) != null) {
                 String[] lineSplit = line.split("\\|");
 
@@ -34,7 +41,9 @@ public class StoreApp {
                 double price = Double.parseDouble(lineSplit[2]);
 
                 Product product = new Product(id, productName, price);
+
                 inventory.add(product);
+
 
             }
             bufferedReader.close();
@@ -45,5 +54,5 @@ public class StoreApp {
         }
         return inventory;
     }
-    }
+}
 
